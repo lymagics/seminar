0724abdffcb9b1b19f2de0ea9c7afebb1c3eddf2
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 10:21:43 +0300

    feat(accounts): add User model.

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,20 @@
+from hashlib import md5
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from model_utils import models as mu
+
+
+class User(mu.TimeStampedModel,
+           mu.SoftDeletableModel,
+           AbstractUser):
+    """
+    User entity.
+    """
+    about_me = models.TextField(default='')
+
+    @property
+    def avatar_url(self) -> str:
+        avatar_hash = md5(self.email.encode()).hexdigest()
+        return f'https://www.gravatar.com/avatar/{avatar_hash}?d=identicon'
