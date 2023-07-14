cc7e51e2e59d751dcbef0fd5a0ce84f05f34c39a
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 14:39:16 +0300

    feat(posts): add Post model.

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from model_utils import models as mu
+
+
+class Post(mu.TimeStampedModel, 
+           models.Model):
+    """
+    Post entity.
+    """
+    text = models.CharField(max_length=180)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+    
+    def __str__(self) -> str:
+        return self.text
+    
+    def get_absolute_url(self) -> str:
+        return reverse('posts:detail', kwargs={'pk': self.pk})
