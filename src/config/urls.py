4689ae930f180c86b9e75412655c1764ae8f0508
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 14:51:14 +0300

    feat(posts): add view to display explore tab.

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
     path('', include('pages.urls')),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from posts.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/list.html'
+
+    def get_queryset(self):
+        return Post.objects.select_related('author')
