18468170de47ee2d85e422d7887aa25b74b1edb1
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 10:26:11 +0300

    feat(accounts): add custom user forms.

---
+++ src/users/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import forms as auth_forms
+
+from users.models import User
+
+
+class UserCreationForm(auth_forms.UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'about_me',)
+
+
+class UserChangeForm(auth_forms.UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'about_me',
+                  'created', 'modified', 'is_removed',)
