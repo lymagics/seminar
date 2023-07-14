7551d08dd68727a0af1db4a0b012f703bf7ca13b
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 17:40:00 +0300

    feat(follows): add follow and unfollow view.

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,6 +5,8 @@ from users import views
 urlpatterns = [
     path('edit/', views.UserUpdateView.as_view(), name='edit'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='detail'),
+    path('<int:pk>/follow/', views.FollowUserView.as_view(), name='follow'),
+    path('<int:pk>/unfollow/', views.UnFollowUserView.as_view(), name='unfollow'),
 ]
 
 app_name = 'users'
--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.detail import DetailView
+from django.http import HttpRequest
+from django.views.generic.base import RedirectView
+from django.views.generic.detail import DetailView, SingleObjectMixin
 from django.views.generic.edit import UpdateView
 
 from users.forms import UserForm
@@ -17,3 +19,33 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_object(self):
         return self.request.user
+
+
+class FollowUserView(LoginRequiredMixin, 
+                     SingleObjectMixin, 
+                     RedirectView):
+    model = User
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        user = self.get_object()
+        request.user.follow(user)
+        return super().get(request, *args, **kwargs)
+    
+    def get_redirect_url(self, *args, **kwargs):
+        user = self.get_object()
+        return user.get_absolute_url()
+
+
+class UnFollowUserView(LoginRequiredMixin, 
+                     SingleObjectMixin, 
+                     RedirectView):
+    model = User
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        user = self.get_object()
+        request.user.unfollow(user)
+        return super().get(request, *args, **kwargs)
+    
+    def get_redirect_url(self, *args, **kwargs):
+        user = self.get_object()
+        return user.get_absolute_url()
