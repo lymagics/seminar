39c15043a3256635bbed0356a3fe51a5682bba5d
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 16:02:16 +0300

    feat(posts): add ability to create new post on home page.

--- src/pages/tests/test_views.py
+++ src/pages/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from django.test import TestCase
 
+import faker
+
 from users.tests.factories import UserFactory
 
 
@@ -9,16 +11,28 @@ class HomePageViewTest(TestCase):
     def setUpTestData(cls):
         cls.url = reverse('pages:home')
         cls.user = UserFactory()
+        cls.faker = faker.Faker()
 
     def test_home_page_view_for_anonymous_user(self):
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        self.assertContains(response, 'Welcome to tunnels!')
+        self.assertEqual(response.status_code, 302)
 
-    def test_home_page_view_for_logged_in__user(self):
+    def test_home_page_view_for_logged_in_user(self):
         self.client.force_login(user=self.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        self.assertContains(response, f'Hello, {self.user.username}')
+        self.assertContains(response, 'What is on your mind?')
+
+    def test_creating_new_post(self):
+        self.client.force_login(user=self.user)
+        new_post = {
+            'text': self.faker.paragraph(),
+        }
+        response = self.client.post(self.url, new_post)
+        self.assertEqual(response.status_code, 302)
+        
+        self.user.refresh_from_db()
+        post = self.user.posts.first()
+        self.assertIsNotNone(post)
+        self.assertEqual(post.text, new_post['text'])
