951bb80af0043bffec4a6f9603f115487fe3c0f3
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 12:40:18 +0300

    test: change tests for home page.

--- src/pages/tests/test_views.py
+++ src/pages/tests/test_views.py
@@ -1,14 +1,24 @@
 from django.urls import reverse
-from django.test import SimpleTestCase
+from django.test import TestCase
+
+from users.tests.factories import UserFactory
 
 
-class HomePageViewTest(SimpleTestCase):
+class HomePageViewTest(TestCase):
     @classmethod
-    def setUpClass(cls):
+    def setUpTestData(cls):
         cls.url = reverse('pages:home')
+        cls.user = UserFactory()
 
-    def test_home_page_view(self):
+    def test_home_page_view_for_anonymous_user(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        self.assertContains(response, 'Home page')
+        self.assertContains(response, 'Welcome to tunnels!')
+
+    def test_home_page_view_for_logged_in__user(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, f'Hello, {self.user.username}')
