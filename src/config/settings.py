6ceff58d56f88686b2fff7ff41825d41c1cba7cf
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 11:13:50 +0300

    feat(pages): add home page view.

--- src/config/settings.py
+++ src/config/settings.py
@@ -43,10 +43,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+TEMPLATES_DIRS = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
