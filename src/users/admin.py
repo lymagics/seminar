1e75437bf6024b6638e9a703ce13873825a3f6fa
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-14 10:53:47 +0300

    feat(accounts): add user model to admin page.

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 
-# Register your models here.
+from users.forms import UserCreationForm, UserChangeForm
+from users.models import User
+
+
+class UserAdmin(auth_admin.UserAdmin):
+    model = User
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ('id', 'username', 'email',)
+    fieldsets = (('Edit user', {
+        'fields': (
+            'email',
+            'username',
+            'about_me',
+            'first_name',
+            'last_name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+        ),
+    }),)
+    add_fieldsets = ((None, {
+        'fields': (
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+    }),)
+
+
+admin.site.register(User, UserAdmin)
