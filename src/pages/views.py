47016a18d039a6d4f4c6720c3e5cb55d959a980d
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-17 17:12:33 +0300

    feat(follows): add following posts to feed page.

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView, View
 from django.views.generic.edit import CreateView
 
 from posts.forms import PostForm
+from posts.models import Post
 
 
 class HomePageGET(TemplateView):
@@ -13,6 +14,15 @@ class HomePageGET(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = PostForm()
+
+        me = self.request.user
+        users = list(me.following.all())
+        users.append(me)
+        posts = Post.objects.filter(
+            author__in=users
+        ).select_related('author').all()
+        context['posts'] = posts
+
         return context
 
 
