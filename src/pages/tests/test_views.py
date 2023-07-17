67535d618ad0cba3bd2857f738754bb79fd54be9
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-17 17:16:07 +0300

    test: add tests for feed page.

--- src/pages/tests/test_views.py
+++ src/pages/tests/test_views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 import faker
 
 from users.tests.factories import UserFactory
+from posts.tests.factories import PostFactory
 
 
 class HomePageViewTest(TestCase):
@@ -18,11 +19,18 @@ class HomePageViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_home_page_view_for_logged_in_user(self):
+        other = UserFactory()
+        post_by_other = PostFactory(author=other)
+        post_by_me = PostFactory(author=self.user)
+        self.user.follow(other)
+
         self.client.force_login(user=self.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'What is on your mind?')
+        self.assertContains(response, post_by_other.text)
+        self.assertContains(response, post_by_me.text)
 
     def test_creating_new_post(self):
         self.client.force_login(user=self.user)
