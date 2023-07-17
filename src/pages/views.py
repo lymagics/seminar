76e99eb0e451cb8bd6894a3a982e58bc56280cbc
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-17 17:23:34 +0300

    refactor: switch from template view to listview.

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,29 +1,30 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View
+from django.views.generic import View
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from posts.forms import PostForm
 from posts.models import Post
+    
 
-
-class HomePageGET(TemplateView):
+class HomePageGET(ListView):
+    model = Post
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = PostForm()
+        return context
 
+    def get_queryset(self):
         me = self.request.user
         users = list(me.following.all())
         users.append(me)
-        posts = Post.objects.filter(
+        return Post.objects.filter(
             author__in=users
         ).select_related('author').all()
-        context['posts'] = posts
-
-        return context
 
 
 class HomePagePOST(CreateView):
