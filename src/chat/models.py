5306b62e4b31df90c25dd812a1e0f627dff6d5dc
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-19 17:00:11 +0300

    feat(chat): add chat and message model/

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,33 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    """
+    Chat entity.
+    """
+    ref = models.UUIDField(default=uuid.uuid4)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                   related_name='chats')
+
+    def __str__(self) -> str:
+        return self.ref
+
+
+class Message(models.Model):
+    """
+    Message entity.
+    """
+    text = models.CharField(max_length=180)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='messages')
+    chat = models.ForeignKey(Chat,
+                             on_delete=models.CASCADE,
+                             related_name='messages')
+
+    def __str__(self) -> str:
+        return self.text[:50]
