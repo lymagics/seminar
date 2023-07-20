924db0aa4dfa2a7560e042475b673ca4cbb9eddb
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-20 14:58:49 +0300

    refactor: add Action view.

---
+++ src/core/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import RedirectView
+
+
+class ActionView(RedirectView):
+    def perform_action(self):
+        raise NotImplementedError
+    
+    def get(self, request, *args, **kwargs):
+        self.perform_action()
+        return super().get(request, *args, **kwargs)
